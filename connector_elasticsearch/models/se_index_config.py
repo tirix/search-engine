881ce0696cef43725bc3106e15bdcf8a0b9c7e35
Thierry Arnoux <thierry.arnoux@gmx.net>
2020-06-25 13:14:45 +0800

    Fix initial SE index config creation
    Fixes from https://github.com/ChinaShrimp.

--- connector_elasticsearch/models/se_index_config.py
+++ connector_elasticsearch/models/se_index_config.py
@@ -1,9 +1,11 @@
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import json
+import logging
 
 from odoo import api, fields, models
 
+_logger = logging.getLogger(__name__)
 
 class SeIndexConfig(models.Model):
 
@@ -15,6 +17,18 @@ class SeIndexConfig(models.Model):
     # This field is used since no widget exists to edit a serialized field
     # into the web fontend
     body_str = fields.Text(compute="_compute_body_str", inverse="_inverse_body_str")
+
+    @api.model
+    def create(self, values):
+        # For new record creation, the inverse function for `body_str` is 
+        # called after the record is inserted into database. The field 
+        # `body` would be empty, and a validation error will pop up as field 
+        # `body` is required. The solution is to override create function, 
+        # and initialize field `body` based on field `body_str`.
+        if 'body' not in values and 'body_str' in values:
+            values['body'] = json.loads(values['body_str'])
+
+        return super(SeIndexConfig, self).create(values)
 
     @api.multi
     @api.depends("body")
